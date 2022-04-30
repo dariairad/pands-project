7493dbe289101800e522449e3c37f911b5146c47
dariagmit <g00411221@gmit.ie>
2022-04-30 21:07:53 +0100

    Dataset upload

---
+++ analysis.py
@@ -0,0 +1,11 @@
+# Analysis of Iris Dataset.
+# Author: Daria Sep
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pan 
+import seaborn as sea
+
+# Importing data from the file.
+
+
