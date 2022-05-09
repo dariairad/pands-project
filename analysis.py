74e5fd19bad6e79a33b12ab420d397fbcf8c7d9f
dariagmit <g00411221@gmit.ie>
2022-05-09 01:26:26 +0100

    code + png files

--- analysis.py
+++ analysis.py
@@ -1,55 +1,131 @@
-# Analysis of Iris Dataset.
+# Analysis of the Iris Dataset.
 # Author: Daria Sep
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pnd 
+import pandas as pnd
 import seaborn as sea
+import sys 
 
 # Importing data from the file.
+file1 = 'iris.data' 
+iris = pnd.read_csv(file1, header =None)
 
-file = 'iris.data' 
-data = pnd.read_csv(file, header =None)
+# setting variables for atributes and class
+sl = 'Sepal Length'
+sw = 'Sepal Width'
+pl = 'Petal Length'
+pw = 'Petal Width'
+sp = 'Species'
 
-#sea.set(style="whitegrid", rc={'figure.figsize':(10,6)}) 
-
-data.columns = ['Sepal Length (cm)','Sepal Width (cm)','Petal Length (cm)','Petal Width (cm)','Species']
 # Adding headers to columns
+iris.columns = [sl, sw, pl, pw, sp]
 
-print('\nAnalysis of the Iris Dataset\n') 
+# Categorizing data by species
+iris_setosa = iris[iris[sp]=='Iris-setosa'] 
+iris_virginica = iris[iris[sp]=='Iris-virginica']
+iris_versicolor = iris[iris[sp]=='Iris-versicolor'] 
 
-print('\nOverview of the Dataset - First 5 and Last 5 Rows\n')
-# Overview of the dataset in a table format
-print(f'{data.head()}\n') 
-print(data.tail())
+# setting style of the grid + colour palette
+pal = ("dodgerblue", "mediumorchid", "teal", "slateblue")
+sea.set(style="darkgrid", palette=pal)
 
-print('\nSample of Data - 5 Random Entries\n')# Random sample of data from the dataset. 
-sample = data.sample(5)
-print(sample) # Outputs five random complete data entries from the dataset
+original_stdout = sys.stdout
 
-print('\nNumber of Rows and Columns\n')
-# Shape of the dataset (rows, columns)
-shape =(data.shape)
-print(f'Rows: {shape[0]}')
-print(f'Columns: {shape[1]}')
+with open("irisSummary.txt", "a") as f: 
 
-print('\nUnique Species Names and Count\n')
-print(data['Species'].value_counts())
+    sys.stdout = f
+    print('\nAnalysis of the Iris Dataset\n') 
+    print ('Basic information\n')
+    print (iris.info()) 
+    #Checking info on dataset: rows, columns, entries, data types, null values
 
-print('\nNull Values\n')
-print (data.isnull().sum())
+    print('\nUnique Species Names and Dataset Balance\n')
+    print(iris[sp].value_counts())
 
-print('\nStatistical Insights\n')
-#The statistical insights
-print(data.describe())
+    print ('\nDuplicate Entires\n')
+    print (iris[iris.duplicated()])
 
-print('\nMedian of Attributes\n') 
-print(data.median()) # Print the median of each of the attributes in tabular form
+    print('\nOverview of the Dataset\n')
+    print('First 5 Rows\n')
+    print(iris.head()) 
+    print('\nRandom Sample - 5 Random Rows\n')# Random sample of data from the dataset. 
+    print (iris.sample(5))
+    # Outputs five random complete data entries from the dataset
 
-print('\nThe Mean Values of the Three Species\n')
-print(data.groupby('Species').mean())
-print('\nThe Correlation Between the Values of the Three Species\n')
-print(data.groupby('Species').corr())
+    print('\nStatistical Insights\n')
+    #The statistical insights
+    print(iris.describe())
 
+    print('\nMedian of Attributes\n') 
+    print(iris.median()) # Print the median of each of the attributes in tabular form
+
+    print('\nThe Mean Values of the Three Species\n')
+    print(iris.groupby(sp).mean())
+    print('\nThe Correlation Between the Values of the Three Species\n')
+    print(iris.groupby(sp).corr())
+
+    sys.stdout = original_stdout
+
+# Species count, further showing the balance of the data base
+
+plt.title('Species Count')
+sea.countplot(x=sp, data=iris)
+plt.savefig('species_count')
+plt.show()
+
+# histogram - general by attribute
+fig, axs = plt.subplots(2, 2, figsize=(8, 8))
+sea.histplot(data=iris, x=sl, color="dodgerblue", ax=axs[0, 0])
+sea.histplot(data=iris, x=sw, color="mediumorchid", ax=axs[0, 1])
+sea.histplot(data=iris, x=pl, color="slateblue", ax=axs[1, 0])
+sea.histplot(data=iris, x=pw, color="teal", ax=axs[1, 1])
+plt.suptitle('Attributes - General')
+plt.savefig('attributes_general')
+
+# defining function for histograms - attributes by species
+def histogram_plot(p1, p2, p3):
+    
+    sea.histplot(data = iris_virginica[p1], label = 'Iris virginica', color = 'dodgerblue') 
+    sea.histplot(data = iris_versicolor[p1], label = 'Iris versicolor', color = 'mediumorchid')
+    sea.histplot(data = iris_setosa[p1], label = 'Iris setosa', color = 'teal')
+    plt.xlabel(p2)
+    plt.ylabel('Count')
+    plt.title('Histogram of ' + p2 + ' by Species') 
+    plt.legend(loc='upper right')
+    plt.savefig(p3)
+
+def histograms():
+    histogram_plot(sl, sl, 'sepal_length_by_species') 
+    histogram_plot(sw, sw, 'sepal_width_by_species')
+    histogram_plot(pl, pl, 'petal_length_by_species')
+    histogram_plot(pw, pw, 'petal_width_by_species')
+
+histograms()
+
+# Attribiutes by species + Outliers
+fig, axes = plt.subplots(2, 2, figsize=(8, 8)) 
+sea.boxplot(x=sp, y=pl,data=iris, ax=axes[0,0])
+sea.boxplot(x=sp, y=sl, data=iris, ax=axes[0,1])
+sea.boxplot(x=sp, y=pw,data=iris, ax=axes[1,0])
+sea.boxplot(x=sp, y=sw, data=iris, ax=axes[1,1])
+plt.suptitle('Attributes by Species + Outliers')
+plt.legend()
+plt.savefig('atributes_outliers')
+
+sea.scatterplot(x=pl, y=pw, hue=sp, data=iris, palette=pal)
+plt.legend(loc='upper left')
+plt.title('Correraltion between Petal Length & Width')
+plt.savefig('petal_length_width')
+
+sea.scatterplot(x=sl, y=sw, hue=sp, data=iris, palette=pal)
+plt.legend(loc='upper right')
+plt.title('Correraltion between Sepal Length & Width')
+plt.savefig('sepal_length_width')
+
+sea.pairplot(data=iris, hue=sp, height=2, palette=pal)
+plt.subplots_adjust(top=0.95)
+plt.suptitle('Relationship Between Attributes by Species')
+plt.savefig('attributes_pairplot')
 
 
